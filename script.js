6a4ef8a29a360d0545b6bdf3caa01d2d5fda1cde
athultony97 <111163968+athultony97@users.noreply.github.com>
2022-12-11 22:37:38 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 // Register the service worker
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
-      navigator.serviceWorker
-        .register("./service-worker.js", {
-          scope: "/pwa-project/"
-        })
-        .then(registration => {
-          console.log("Service Worker registered: ", registration)
-        })
-        .catch(registrationError => {
-          console.log("Service Worker registration failed: ", registrationError)
-        })
+        navigator.serviceWorker
+            .register("./service-worker.js", {
+                scope: "/pwa-project/"
+            })
+            .then(registration => {
+                console.log("Service Worker registered: ", registration)
+            })
+            .catch(registrationError => {
+                console.log("Service Worker registration failed: ", registrationError)
+            })
     })
-  }
-  
-  function onClickHome() {
-    myNavigator.pushPage("index1.html")
-  }
+}
+
+
+var button = getElementById('btn')
+button.addEventListener("click", myFunction);
+
+function myFunction() {
+    document.pushPage("https://athultony97.github.io/pwa-project/index1.html");
+}
 
 // disabling the form section initally
 document.getElementById("form-section").style.display = "none"
@@ -27,7 +31,7 @@ function showFormSec() {
 }
 
 
-  if ("Notification" in window && "serviceWorker" in navigator) {
+if ("Notification" in window && "serviceWorker" in navigator) {
     document.getElementById("additems").addEventListener("click", function () {
         var permissionStatus = Notification.permission;
         console.log("Notification permission status: ", permissionStatus)
@@ -63,8 +67,8 @@ function showFormSec() {
             };
             new Notification(document.getElementById("item").value)
             //new Notification(document.getElementById("body").value)
-             navigator.serviceWorker.ready.then((register) => {
-             register.showNotification(item.value, options);
+            navigator.serviceWorker.ready.then((register) => {
+                register.showNotification(item.value, options);
             });
         }
     });
@@ -75,14 +79,16 @@ function showFormSec() {
 }
 
 // firebase configuration
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  doc,
-  deleteDoc,
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    doc,
+    deleteDoc,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -93,54 +99,56 @@ const firebaseConfig = {
     messagingSenderId: "550978748884",
     appId: "1:550978748884:web:38d4e8ad677718e21e5641",
     measurementId: "G-RQMK4T7N3W"
-  };
+};
 
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-  var item = document.getElementById("item");
-  var error = document.getElementsByClassName("error");
+var item = document.getElementById("item");
+var error = document.getElementsByClassName("error");
 
-  // Loading the music from the database
-  window.onload = getItem();
+// Loading the music from the database
+window.onload = getItem();
 
-  // get the songs from the database
-    function getItem() {
+// get the songs from the database
+function getItem() {
     getDocs(collection(db, "shopperlist")).then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        displaySongs(doc.data(), doc.id);
-      });
+        querySnapshot.forEach((doc) => {
+            displaySongs(doc.data(), doc.id);
+        });
     });
-  }
+}
 
-  // adding items and validation
+// adding items and validation
 var addItem = document.getElementById("add_btn");
 addItem.addEventListener("click", async () => {
-  if (item.value == "") {
-    error[0].innerHTML = "Please enter the item";
-    error[0].style.color = "red";
-  }
-  if (item.value != "") {
-    error[0].innerHTML = "";
-    error[1].innerHTML = "";
-    try {
-      const docRef = await addDoc(collection(db, "shopperlist"), {
-        item: item.value,
-      });
-      console.log("Document written with ID: ", docRef.id);
-      displayItems({ item: item.value }, docRef.id);
-      clearFields();
-    } catch (e) {
-      console.error("Error adding document: ", e);
+    if (item.value == "") {
+        error[0].innerHTML = "Please enter the item";
+        error[0].style.color = "red";
     }
-  }
-  //disappear the error message after 3 seconds
-  setTimeout(() => {
-    error[0].innerHTML = "";
-    error[1].innerHTML = "";
-  }, 3000);
+    if (item.value != "") {
+        error[0].innerHTML = "";
+        error[1].innerHTML = "";
+        try {
+            const docRef = await addDoc(collection(db, "shopperlist"), {
+                item: item.value,
+            });
+            console.log("Document written with ID: ", docRef.id);
+            displayItems({
+                item: item.value
+            }, docRef.id);
+            clearFields();
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    }
+    //disappear the error message after 3 seconds
+    setTimeout(() => {
+        error[0].innerHTML = "";
+        error[1].innerHTML = "";
+    }, 3000);
 });
 
 
@@ -161,4 +169,4 @@ function displayItems(data, id) {
       `;
     itemList[0].appendChild(itemDivision);
     deleteBtn(id, itemDivision.querySelector(".buttons"));
-  }
+}
